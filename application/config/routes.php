39c1d99ba0747659d167aad56e0cad72a080325b
developerprince <N01416165H@students.nust.ac.zw>
2018-08-02 13:48:23 +0200

    Startup Central Definition

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['user'] = 'user/login';
+$route['startup_cen/globalnews'] = 'dashboard/globalnews';
+$route['startup_cen/localnews'] = 'dashboard/localnews';
+$route['startup_cen/market'] = 'dashboard/market';
+$route['startup_cen/profile'] = 'dashboard/profile';
+$route['startup_cen/list'] = 'dashboard/startups';
 $route['startup_cen'] = 'dashboard/index';
+$route['user'] = 'user/login';
 $route['user/register'] = 'user/register';
 $route['startups/register'] = 'startup/upload';
 $route['startups'] = 'startup/index';

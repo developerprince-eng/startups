8e3efa701ca82c7e72cab60721c4eaea197f7046
developerprince <N01416165H@students.nust.ac.zw>
2018-08-03 12:55:37 +0200

    exchange rate updates

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,11 +8,11 @@
 ini_set("allow_url_fopen", 1);
  class Dashboard extends CI_Controller{
 	public function index(){
-		/*
+		
 		if(!$this->session->userdata('logged_in')){
 			redirect('user');
 		}
-		*/
+		
         
         function CallAPI($method, $url, $data = false)
         {
@@ -48,22 +48,13 @@ ini_set("allow_url_fopen", 1);
             return $result;
         }
 
-
         $url_g = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=2126e74d57f64166a50d6302a83c18be';
 		
 		$url_b = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=b5f55194d174421ab6ad8bf68d7fcc8b';
 
-        $url_xrate1 = 'https://exchangeratesapi.io/api/latest?base=USD';
-
-
-
 		$response_3 = CallAPI(null, $url_b, false);
 
-        $response_2 = CallAPI(null, $url_xrate1, false);
-
         $response_1 = CallAPI(null, $url_g, false);
-
-        $bit2usd = json_decode($response_2, true);
         
 		$articles = json_decode($response_1, true);
 	
@@ -76,8 +67,6 @@ ini_set("allow_url_fopen", 1);
 		$data['user'] = $num_usr;
 
 		$data['startup'] = $num_su;
-
-        $data['btc2usd'] = $bit2usd;
 
 		$data['articles'] = $articles['articles'];
 
@@ -163,6 +152,47 @@ ini_set("allow_url_fopen", 1);
 			redirect('user');
 		}
 		
+		function CallAPI($method, $url, $data = false)
+        {
+            $curl = curl_init();
+
+            switch ($method)
+            {
+                case "POST":
+                    curl_setopt($curl, CURLOPT_POST, 1);
+
+                    if ($data)
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                    break;
+                case "PUT":
+                    curl_setopt($curl, CURLOPT_PUT, 1);
+                    break;
+                default:
+                    if ($data)
+                        $url = sprintf("%s?%s", $url, http_build_query($data));
+            }
+
+            // Optional Authentication:
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+            $result = curl_exec($curl);
+
+            curl_close($curl);
+
+            return $result;
+        }
+
+		$url_xrate1 = 'https://exchangeratesapi.io/api/latest?base=USD';
+
+		$response_2 = CallAPI(null, $url_xrate1, false);
+
+		$xrate = json_decode($response_2, true);
+
+		$data['xrates'] = $xrate;
 
 		$this->load->view('templates/dashboard_header');
 		$this->load->view('dashboard/market', $data);
